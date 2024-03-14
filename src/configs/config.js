e7c93fee257c1f34dc8c51f9626c7c606f7e029a
Wilian Brito <contato.beitsoft@gmail.com>
2024-03-14 12:03:24 -0300

    add news features

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,8 +1,8 @@
 const config = {
-  user: 'wilian',
-  password: 'willkah1',
-  server: 'WIL-BRIT0\\SQLEXPRESS',
-  database: 'DGCS_ERP',
+  user: '',
+  password: '',
+  server: '',
+  database: '',
   port: 1433,
   options: { encrypt: true, trustServerCertificate: true }
 };
