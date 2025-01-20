1f49532af7e9be954022fd173f3ee12bb08fe667
Wilian Brito <contato.beitsoft@gmail.com>
2025-01-20 16:51:20 -0300

    alterado arquivo para testar conexão

--- src/models/testConnectionModels.js
+++ src/models/testConnectionModels.js
@@ -2,16 +2,16 @@ import config from "../config.js";
 import sql from "mssql";
 
 async function testConnectionDatabase() {
-
-    try {
-      await sql.connect(config);
-    } catch (error) {
-      throw error;
-    } finally {
-      await sql.close();
-      console.log("Conexão fechada");
-    }
-
+  try {
+    const pool = await sql.connect(config);
+    console.log("Conexão com o banco de dados bem-sucedida!");
+    await pool.close(); // Fecha a conexão
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error.message);
+    throw error;
+  } finally {
+    console.log("Conexão encerrada.");
+  }
 }
 
 export default { testConnectionDatabase };
